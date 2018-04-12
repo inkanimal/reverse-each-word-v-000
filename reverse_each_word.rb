26cdb03d5a616ee5665c116c90a516473305be12
geoffrey woodcock <theatomproject@gmail.com>
2018-04-12 20:12:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(string)
      puts "#{word.reverse}"
    end
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = array.collect{|a| a.reverse}
+  new_array.join(" ")
+end
