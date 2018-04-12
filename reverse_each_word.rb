08ff45e021993f436d0b48993ce88a250ece0114
geoffrey woodcock <theatomproject@gmail.com>
2018-04-12 20:13:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word(string)
-   string.each do |word|
-     puts "#{word.reverse}"
-   end
-end
-
-def reverse_each_word(string)
   array = string.split(" ")
   new_array = array.collect{|a| a.reverse}
   new_array.join(" ")
